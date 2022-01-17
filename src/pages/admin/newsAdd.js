211658b4c324656dd2f44928ebee41376661f42c
HiamBi <ngongquan@gmail.com>
2022-01-17 11:49:33 +0700

    delete error code

--- src/pages/admin/newsAdd.js
+++ src/pages/admin/newsAdd.js
@@ -1,6 +1,6 @@
 import FormNews from '../../component/admin/formNews';
 import SideBar from '../../component/admin/sidebar';
-import { listItem } from '../../data';
+// import { listItem } from '../../data';
 
 const NewsAdd = {
   print() {
@@ -11,27 +11,25 @@ const NewsAdd = {
    </div>`;
   },
   afterPrint() {
-    const btns = document.querySelectorAll('#btn-submit');
-    btns.forEach((btn) => {
-      const { id } = btn.dataset;
-      btn.addEventListener('Click', () => {
-        if (id === '') {
-          const newTitle = document.getElementById('title').value;
-          const describe = document.getElementById('describe').value;
-          const newId = listItem.length + 1;
-          const newPost = {
-            id: newId,
-            title: newTitle,
-            desc: describe,
-            createAt: ' ',
-            img: ' ',
-          };
-          listItem.push(newPost);
-          return console.log(listItem);
-        }
-        return console.log(listItem);
-      });
-    });
+    // const btns = document.querySelectorAll('#btn-submit');
+    // btns.forEach((btn) => {
+    //   const { id } = btn.dataset;
+    //   btn.addEventListener('Click', () => {
+    //     if (id === '') {
+    //       const newTitle = document.getElementById('title').value;
+    //       const describe = document.getElementById('describe').value;
+    //       const newId = listItem.length + 1;
+    //       const newPost = {
+    //         id: newId,
+    //         title: newTitle,
+    //         desc: describe,
+    //         createAt: ' ',
+    //         img: ' ',
+    //       };
+    //       listItem.push(newPost);
+    //     }
+    //   });
+    // });
   },
 };
 export default NewsAdd;
